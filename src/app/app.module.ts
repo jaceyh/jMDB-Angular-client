22b3d675548adc0d16b8f69c0bfc74edd0a2c289
jaceyh <jacey.hallock@gmail.com>
2024-02-02 08:38:17 -0500

    clean code in app.module and update readme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-//import material features
-import { MatInputModule } from '@angular/material/input';
+// import material features
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
-import { FormsModule } from '@angular/forms';
-import { RegistrationFormComponent } from './registration-form/registration-form.component';
+
+// import app components
+import { AppComponent } from './app.component';
+import { DirectorDialogComponent } from './director-dialog/director-dialog.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
-import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { MovieDetailsDialogComponent } from './movie-details-dialog/movie-details-dialog.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { TagDialogComponent } from './tag-dialog/tag-dialog.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserUpdateFormComponent } from './user-update-form/user-update-form.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { DirectorDialogComponent } from './director-dialog/director-dialog.component';
-import { TagDialogComponent } from './tag-dialog/tag-dialog.component';
-import { MovieDetailsDialogComponent } from './movie-details-dialog/movie-details-dialog.component';
-
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 const appRoutes: Routes = [
     { path: 'welcome', component: WelcomePageComponent },
